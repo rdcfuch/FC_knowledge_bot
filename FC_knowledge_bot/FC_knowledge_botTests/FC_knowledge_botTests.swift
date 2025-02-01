53f8cfcd61a998b2ab92b82449b83bb780d133b6
rdcfuch <rdcfuch@yahoo.com>
2025-01-31 16:07:33 -0800

    new project created

---
+++ FC_knowledge_bot/FC_knowledge_botTests/FC_knowledge_botTests.swift
@@ -0,0 +1,17 @@
+//
+//  FC_knowledge_botTests.swift
+//  FC_knowledge_botTests
+//
+//  Created by FC Fu on 1/31/25.
+//
+
+import Testing
+@testable import FC_knowledge_bot
+
+struct FC_knowledge_botTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
